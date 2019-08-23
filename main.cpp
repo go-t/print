44458395712d66048b83c879a70399af7f003692
go-t <shushengsky@163.com>
2019-08-23 21:25:08 +0800

    Create LICENSE
    Add CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <sstream>
 #include <stdlib.h>
 #include <string>
@@ -11,58 +10,67 @@
 
 using namespace print_helper;
 
-static_assert(_not<std::false_type>::value, "");
-static_assert(_not<_not<std::true_type>>::value, "");
+// static_assert(_not<std::false_type>::value, "");
+// static_assert(_not<_not<std::true_type>>::value, "");
 
 
-static_assert(_and<std::true_type, std::true_type>::value, "");
-static_assert(!_and<std::false_type, std::true_type>::value, "");
-static_assert(!_and<std::false_type, std::false_type>::value, "");
+// static_assert(_and<std::true_type, std::true_type>::value, "");
+// static_assert(!_and<std::false_type, std::true_type>::value, "");
+// static_assert(!_and<std::false_type, std::false_type>::value, "");
 
-static_assert(!_is_pointer<int>::value, "");
-static_assert(_is_pointer<int*>::value, "");
-static_assert(_is_pointer<void*>::value, "");
+// static_assert(!_is_pointer<int>::value, "");
+// static_assert(_is_pointer<int*>::value, "");
+// static_assert(_is_pointer<void*>::value, "");
 
-static_assert(!_is_std_string<int>::value, "");
-static_assert(!_is_std_string<const char*>::value, "");
-static_assert(_is_std_string<std::string>::value, "");
-static_assert(_is_std_string<std::wstring>::value, "");
-static_assert(!_is_std_string<std::vector<int>>::value, "");
+// static_assert(!_is_std_string<int>::value, "");
+// static_assert(!_is_std_string<const char*>::value, "");
+// static_assert(_is_std_string<std::string>::value, "");
+// static_assert(_is_std_string<std::wstring>::value, "");
+// static_assert(!_is_std_string<std::vector<int>>::value, "");
 
 static_assert(!_is_std_container<int>::value, "");
 static_assert(!_is_std_container<const char*>::value, "");
-static_assert(!_is_std_container<std::string>::value, "");
+// static_assert(!_is_std_container<std::string>::value, "");
 static_assert(_is_std_container<std::vector<int>>::value, "");
 
-static_assert(_is_printable_helper<int, std::ostream>::value, "");
-static_assert(_is_printable_helper<const char*, std::ostream>::value, "");
-static_assert(_is_printable_helper<std::string, std::ostream>::value, "");
-static_assert(!_is_printable_helper<std::vector<int>, std::ostream>::value, "");
+// static_assert(_is_printable_helper<int, std::ostream>::value, "");
+// static_assert(_is_printable_helper<const char*, std::ostream>::value, "");
+// static_assert(_is_printable_helper<std::string, std::ostream>::value, "");
+// static_assert(!_is_printable_helper<std::vector<int>, std::ostream>::value, "");
 
 
-static_assert(!is_printable_pointer<int>::value, "");
-static_assert(is_printable_pointer<const int*>::value, "");
-static_assert(!is_printable_pointer<const void*>::value, "");
-static_assert(is_opaque_pointer<const void*>::value, "");
-static_assert(!is_opaque_pointer<const int*>::value, "");
+// static_assert(!is_printable_pointer<int>::value, "");
+// static_assert(is_printable_pointer<const int*>::value, "");
+// static_assert(!is_printable_pointer<const void*>::value, "");
+// static_assert(is_opaque_pointer<const void*>::value, "");
+// static_assert(!is_opaque_pointer<const int*>::value, "");
 
 
-static_assert(_is_printable<int>::value, "");
-static_assert(_is_printable<std::string>::value, "");
-static_assert(!_is_printable<bool>::value, "");
-static_assert(!_is_printable<int*>::value, "");
-static_assert(!_is_printable<std::vector<int>>::value, "");
+// static_assert(_is_printable<int>::value, "");
+// static_assert(_is_printable<std::string>::value, "");
+// static_assert(!_is_printable<bool>::value, "");
+// static_assert(!_is_printable<int*>::value, "");
+// static_assert(!_is_printable<std::vector<int>>::value, "");
 
 int main()
 {
     int a = 456;
     int* pa = &a;
     int** ppa = &pa;
+
+    bool ok = true;
+
+#ifdef _MSC_VER
+    println("_MSC_VER =", _MSC_VER);
+#endif // _MSC_VER
+    println("__cplusplus =", __cplusplus);
     
     std::map<std::string, double> dict{{"A", 1.1}, {"B", 2.2}};
     
-    println(2.2);
-    println(true, 123, 'A', "hello", &ppa);
+    println(ok, &ok);
+    println(123, 2.3); 
+    println('A', "hello", std::string("world"));
+    println(a, pa, ppa, (double*)nullptr);
     println(std::make_pair(1,2.0), std::make_pair("PI", 3.14159));
     println(std::vector<int>{1, 2, 3}, std::list<std::string>{"alpha", "beta"});
     println(dict, &dict, std::set<char>{'A', 'B'}); 
--- print.hpp
+++ print.hpp
@@ -1,156 +1,74 @@
 #ifndef _PRINT_HPP_
 #define _PRINT_HPP_
 
+#include <iostream> // std::cout
+#include <sstream>  // std::ostringstream
 #include <type_traits>
-#include <sstream>
 
-namespace std {
-extern std::ostream cout;     
-}
+#if (__cplusplus < 201402L) && (_MSC_VER < 1900)
+#error "c++14 standard requested!"
+#endif
 
 namespace print_helper {
 
 // -- _void_t(std::void_t)
-
-template< class... > using _void_t = void;
-
-// -- _not(std::negation) --
-
-template<class B> struct _not : std::integral_constant<bool, !bool(B::value)> { };
-
-// -- _and(std::conjunction) --
-
-template<class...> struct _and : std::true_type { };
-template<class B1> struct _and<B1> : B1 { };
-template<class B1, class... Bn>
-struct _and<B1, Bn...>: std::conditional_t<bool(B1::value), _and<Bn...>, B1> {};
-
-// -- _is_pointer --
-
-template <class T, class = _void_t<>> 
-struct _is_pointer : std::is_pointer<T> {}; 
-
-
-// -- _is_std_string --
-
-template <class T, class = _void_t<>> 
-struct _is_std_string : std::false_type {}; 
-
-template <class T> 
-struct _is_std_string<T, _void_t<decltype(std::declval<T &>().c_str())>> : std::true_type {};
+template <class...> using _void_t = void;
 
 // -- _is_std_container --
+template <class T, class = _void_t<>> struct _is_std_container : std::false_type {};
 
-template <class T, class = _void_t<>> 
-struct _is_std_container : std::false_type {}; 
+template <class T>
+struct _is_std_container<T, _void_t<typename T::iterator>> : std::true_type {};
 
-template <class T> 
-struct _is_std_container<T, _void_t<typename T::iterator> > : _not<_is_std_string<T>> {};
-
-// -- _is_printable_helper --
-
+// -- _is_streamable_helper --
 template <class T, class OS, class = _void_t<>>
-struct _is_printable_helper : std::false_type {};
+struct _is_streamable_helper : std::false_type {};
 
 template <class T, class OS>
-struct _is_printable_helper<T, OS, _void_t<decltype(std::declval<OS &>() << std::declval<const T &>())>> : std::true_type {};
+struct _is_streamable_helper<
+    T, OS, _void_t<decltype(std::declval<OS &>() << std::declval<const T &>())>>
+    : std::true_type {};
 
-// -- _is_printable --
-
-template <class T>
-struct _is_printable: _and<_is_printable_helper<T, std::ostream>, _not<std::is_same<T, bool>>, _not<_is_pointer<T>>>{};
-
-
+// -- _is_streamable --
 
 template <class T>
-using _enable_if =
-    typename std::enable_if<T::value, std::ostream>::type &;
+struct _is_streamable
+    : std::integral_constant<bool, bool(_is_streamable_helper<T, std::ostream>::value &&
+                                        !std::is_same<T, bool>::value) &&
+                                       !std::is_pointer<T>::value> {};
+
+template <class T>
+using _enable_if = typename std::enable_if<T::value, std::ostream>::type &;
 
 // -- declare --
+
+template <class T, size_t N> std::ostream &_print(std::ostream &os, const T (&v)[N]);
+
+std::ostream &_print(std::ostream &os, const char *v);
+std::ostream &_print(std::ostream &os, const std::string s);
+
+template <class T> std::ostream &_print(std::ostream &os, const T *p);
+
 template <class K, class V>
 std::ostream &_print(std::ostream &os, const std::pair<K, V> &v);
 
-template <class C>
-_enable_if<_is_std_container<C>> _print(std::ostream &os, const C &c);
+template <class T> _enable_if<std::is_same<T, bool>> _print(std::ostream &os, const T &v);
 
+template <class T> _enable_if<_is_streamable<T>> _print(std::ostream &os, const T &v);
 
-std::ostream& _print(std::ostream &os, const char* v);
-
-template <class T>
-_enable_if<_is_printable<T>> _print(std::ostream &os, const T &v);
-
-template <class T, size_t N>
-std::ostream &_print(std::ostream &os, const T (&v)[N]);
-
-template <class T>
-_enable_if<_is_printable<T>> _print(std::ostream &os, const T &v);
-
-std::ostream& _print(std::ostream &os, const char* v);
-
-template <class T>
-_enable_if<std::is_same<T, bool>> _print(std::ostream &os, const T &v);
+template <class C> _enable_if<_is_std_container<C>> _print(std::ostream &os, const C &c);
 
 // -- is_printable_pointer --
 
-template <class T, class = _void_t<>>
-struct is_printable_pointer : std::false_type {};
+template <class T, class = _void_t<>> struct is_printable : std::false_type {};
 
 template <class T>
-struct is_printable_pointer<T*, _void_t<decltype(_print(std::declval<std::ostream&>() , std::declval<const T&>()))>> : std::true_type {};
-
-template <class T>
-struct is_opaque_pointer: _and<_is_pointer<T>, _not<is_printable_pointer<T>>> {};
-
+struct is_printable<T, _void_t<decltype(_print(std::declval<std::ostream &>(),
+                                               std::declval<const T &>()))>>
+    : std::true_type {};
 
 // -- define --
-
-template <class T>
-_enable_if<std::is_same<T, bool>> _print(std::ostream &os, const T &v) {
-    return os << (v ? "true" : "false");
-}
-
-std::ostream& _print(std::ostream &os, const char* v) {
-    return os << v;
-}
-
-template <class T>
-_enable_if<_is_printable<T>> _print(std::ostream &os, const T &v) {
-    return os << v;
-}
-
-template <class T, size_t N>
-std::ostream &_print(std::ostream &os, const T (&v)[N]) {
-    os << "[" << N << "]{";
-    for (size_t i = 0; i < N; i++) {
-        if (i > 0) {
-            os << ", ";
-        }
-        _print(os, v[i]);
-    }
-    return os << "}";
-}
-
-template <class T>
-_enable_if<is_printable_pointer<T>> _print(std::ostream &os, const T &v) {
-    if (v == nullptr) {
-        return os << "<nil>";
-    } else {
-        return _print(os << "*", *v);;
-    }
-}
-
-// TODO: opaque pointer
-template <class T>
-_enable_if<is_opaque_pointer<T>> _print(std::ostream &os, const T &v) {
-    if (v == nullptr) {
-        return os << "<nil>";
-    } else {
-        return os << std::hex << v;
-    }
-}
-
-template <class C>
-_enable_if<_is_std_container<C>> _print(std::ostream &os, const C &c) {
+template <class C> _enable_if<_is_std_container<C>> _print(std::ostream &os, const C &c) {
     os << "{";
     auto it = c.begin();
     if (it != c.end()) {
@@ -162,36 +80,69 @@ _enable_if<_is_std_container<C>> _print(std::ostream &os, const C &c) {
     return os << "}";
 }
 
+template <class T> _enable_if<_is_streamable<T>> _print(std::ostream &os, const T &v) {
+    return os << v;
+}
+
+template <class T>
+_enable_if<std::is_same<T, bool>> _print(std::ostream &os, const T &v) {
+    return os << (v ? "true" : "false");
+}
+
 template <class K, class V>
 std::ostream &_print(std::ostream &os, const std::pair<K, V> &v) {
     return _print(_print(os, v.first) << ": ", v.second);
 }
 
+template <class T> std::ostream &_print(std::ostream &os, const T *p) {
+    if (p == nullptr) {
+        return os << "<nil>";
+    } else if (std::is_pointer<std::remove_reference_t<T>>::value ||
+               is_printable<T>::value) {
+        return _print(os << "*", *p);
+    } else {
+        return os << "*" << std::hex << p;
+    }
+}
+
+template <class T, size_t N> std::ostream &_print(std::ostream &os, const T (&v)[N]) {
+    os << "[" << N << "]{";
+    for (size_t i = 0; i < N; i++) {
+        if (i > 0) {
+            os << ", ";
+        }
+        _print(os, v[i]);
+    }
+    return os << "}";
+}
+
+std::ostream &_print(std::ostream &os, const char *v) { return os << v; }
+std::ostream &_print(std::ostream &os, const std::string s) { return os << s; }
+
 } // namespace print_helper
 
-template <class T>
-std::ostream &oprint(std::ostream &os, const T &v) {
+template <class T> std::ostream &osprint(std::ostream &os, const T &v) {
     return print_helper::_print(os, v);
 }
 
-// oprint(const T&...)
+// osprint(const T&...)
 template <class T0, class... Args>
-std::ostream &oprint(std::ostream &os, const T0 &v0, Args &&... args) {
-    return oprint(oprint(os, v0) << ", ", std::forward<Args>(args)...);
+std::ostream &osprint(std::ostream &os, const T0 &v0, Args &&... args) {
+    return osprint(osprint(os, v0) << " ", std::forward<Args>(args)...);
 }
 
-template <class... Args>
-std::string sprint(Args &&... args) {
+template <class... Args> std::string sprint(Args &&... args) {
     std::ostringstream oss;
-    oprint(oss, std::forward<Args>(args)...);
+    osprint(oss, std::forward<Args>(args)...);
     return oss.str();
 }
 
-template <class... Args>
-void print(Args &&... args) { oprint(std::cout, std::forward<Args>(args)...); }
+template <class... Args> void print(Args &&... args) {
+    osprint(std::cout, std::forward<Args>(args)...);
+}
 
-template <class... Args>
-void println(Args &&... args) { oprint(std::cout, std::forward<Args>(args)...) << std::endl; }
-
+template <class... Args> void println(Args &&... args) {
+    osprint(std::cout, std::forward<Args>(args)...) << std::endl;
+}
 
 #endif // _PRINT_HPP_
